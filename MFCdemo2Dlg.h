cb77bc84bab3e173e09336c2c74ddaf8a3d48365
jojolee123 <56385650+jojolee123@users.noreply.github.com>
2021-12-25 12:10:24 +0800

    Add files via upload

---
+++ MFCdemo2Dlg.h
@@ -0,0 +1,73 @@
+﻿
+// MFCdemo2Dlg.h: 头文件
+//
+
+#pragma once
+#include "MVGigE.h"
+#include "MVImage.h"
+#include "MVCamProptySheet.h"
+
+//int uncharToMat(uchar* p2, Mat& Src, int flag);
+// CMFCdemo2Dlg 对话框
+class CMFCdemo2Dlg : public CDialogEx
+{
+
+// 构造
+public:
+	CMFCdemo2Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCDEMO2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+public:
+	//定义相机句柄对象
+	HANDLE m_hCam;
+	HANDLE m_hPropDlg;
+	//定义图像处理对象
+	MVImage m_image;
+	//定义像素格式对象
+	MV_PixelFormatEnums m_PixelFormat;
+	//采集标识
+	bool m_bRun;
+	//通过回调函数获取图像数据信息
+	int OnStreamCB(MV_IMAGE_INFO* pInfo);
+	//画图
+	void DrawImage();
+
+	//关闭相机、释放资源
+	void OnClose();
+
+	//afx_msg void OnBnClickedOpencam();
+	//afx_msg void OnBnClickedStartgrab();
+	//afx_msg void OnBnClickedClosecam();
+	void EnableButton();
+	void OpenCamera();
+	afx_msg void OnBnClickedFreeRun();
+	afx_msg void OnBnClickedStop();
+	afx_msg void OnBnClickedSave();
+	afx_msg void OnBnClickedProperties();
+	afx_msg void OnBnClickedStartrun();
+
+	//afx_msg void DrawImage();
+
+
+
+
+};
---
+++ resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCdemo2.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCDEMO2_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDS_NO_CAM                      129
+#define IDS_ERR_ACCESS_DENY             130
+#define IDS_ERR_OPEN                    131
+#define IDS_PROMPT                      132
+#define IDS_PROPERTY                    133
+#define IDS_UNNAMED                     134
+#define IDS_ERR_SINGLE_GRAB             135
+#define IDC_FREE_RUN                    1001
+#define IDC_STOP                        1002
+#define IDC_PROPERTIES                  1003
+#define IDC_SAVE                        1005
+#define IDC_STARTRUN                    1006
+#define IDC_DISPLAY_IMAGE               1009
+#define IDC_EDIT1                       1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
